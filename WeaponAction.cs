a4e462f325aeb42135a4c8b64d9d23912b217f01
StefanX <60297289+stefanx111@users.noreply.github.com>
2026-04-21 11:47:06 +0300

    fix(agents): fix model

--- WeaponAction.cs
+++ WeaponAction.cs
@@ -491,7 +491,7 @@ namespace WeaponPaints
 				Server.NextFrame(() =>
 				{
 					player.PlayerPawn.Value.SetModel(
-						$"characters/models/{model}.vmdl"
+						$"agents/models/{model}.vmdl"
 					);
 				});
 			}
